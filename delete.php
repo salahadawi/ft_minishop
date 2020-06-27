0e5dae319a5a47a9e309e87ee836261c3e0a930c
Camilla Leisti <camilla.leisti@gmail.com>
2020-06-27 16:51:15 +0300

    delete.php minor change

--- delete.php
+++ delete.php
@@ -17,10 +17,8 @@ if ($_POST['submit'] === 'Delete' && auth($_SESSION['logged_on_user'], $_POST['p
 			array_splice($array, $i, 1);
 			break ;
 		}
-		echo "!\n";
 		$i++;
 	}
-	print_r($array);
 	$data = serialize($array);
 	file_put_contents($filename, $data);
 }
