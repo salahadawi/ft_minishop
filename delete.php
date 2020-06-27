ec546e2497e24cf5eb1e650a5a42010b97885d0d
Camilla Leisti <camilla.leisti@gmail.com>
2020-06-27 16:47:18 +0300

    user delete

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'auth.php';
+header("Location: logout.php");
+session_start();
+if ($_POST['submit'] === 'Delete' && auth($_SESSION['logged_on_user'], $_POST['passwd']))
+{
+	echo "here\n";
+	$filename = '../private/passwd';
+    $str = file_get_contents($filename);
+	$array = unserialize($str);
+	$i = 0;
+
+    foreach ($array as $key => $value)
+    {
+        if ($value['login'] === $_SESSION['logged_on_user'])
+		{	
+			array_splice($array, $i, 1);
+			break ;
+		}
+		echo "!\n";
+		$i++;
+	}
+	print_r($array);
+	$data = serialize($array);
+	file_put_contents($filename, $data);
+}
+?>
