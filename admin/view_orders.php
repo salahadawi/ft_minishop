f0f3256bff9395b0bb90473653148264dd2d25ce
sadawi <hahli144@hotmail.com>
2020-06-28 14:19:12 +0300

    create page to view placed orders

--- admin/view_orders.php
+++ admin/view_orders.php
@@ -1,9 +1,31 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html>
 <body>
 <p>View orders</p>
+<?php
+
+include("../functions.php");
+session_start();
+$directory = "../orders";
+$dir = array_diff(scandir("../orders"), array('..', '.'));
+foreach ($dir as $file)
+{
+	$name = substr($file, 0, strpos($file, "_order"));
+	print($name."'s order: <br />");
+	$cart = csv_to_array($directory."/".$file);
+	$total_price = 0;
+	foreach ($cart as $item)
+		{
+			echo $item['name'].", quantity: ".$item['quantity']."<br />";
+			$total_price += $item['price'] * $item['quantity'];
+		}
+		echo "<br /> Total price: ".$total_price."$ <br /><br /><br />";
+}
+if (empty($dir))
+	print("No orders have been placed yet!")
+?>
+<form action="admin.php">
+    <input type="submit" value="Return" />
+</form>
 </body>
 </html>
