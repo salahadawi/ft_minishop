cdae77516fef2ed24c43097ca37a1f812b17bc20
sadawi <hahli144@hotmail.com>
2020-06-28 16:24:43 +0300

    add ability to manage products

--- admin/edit_category.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-<body>
-<p>Edit category</p>
-</body>
-</html>
--- admin/edit_product.php
+++ admin/edit_product.php
@@ -1,26 +1,65 @@
-<?php
-session_start();
-
-// Open the file for reading
-if (($h = fopen("products.csv", "r")) !== FALSE) 
-{
-  // Convert each line into the local $data variable
-  while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
-  {		
-    // Read the data from a single line
-  }
-
-  // Close the file
-  fclose($h);
-}
-
-?>
 <!DOCTYPE html>
 <html>
 <body>
-	<h2>Edit product</h2>
+	<h2>Manage products</h2>
 	<form action="admin.php">
         <input type="submit" value="Return" />
 	</form>
 </body>
 </html>
+
+<?php
+
+include("../functions.php");
+session_start();
+
+$products = csv_to_array_with_format("../products.csv");
+$format = array_shift($products);
+
+if ($_GET['submit'])
+{
+	foreach ($products as &$item)
+	{
+		if ($_GET['submit'] === $item['name'])
+		{
+			if ($_GET['quantity'] == 0)
+				$item = NULL;
+			else
+			{
+				$item['name'] = $_GET['name'];
+				$item['price'] = $_GET['price'];
+				$item['quantity'] = $_GET['quantity'];
+				$item['category'] = $_GET['category'];
+			}
+		}
+	}
+	unset($item);
+	$handle = fopen("../products.csv", "w");
+	fputcsv($handle, $format);
+	foreach ($products as $item)
+	{
+		fputcsv($handle, $item);
+	}
+}
+
+foreach ($products as $item)
+	{
+		if (!$_SESSION['category'] || strpos($item['category'], $_SESSION['category']) !== false)
+		{
+			?>
+			<form action="edit_product.php" method="GET">
+			<br /> Name: 
+			<input type="text" name="name" value= <?= $item['name'] ?>>
+			<br /> Price: 
+			<input type="number" name="price" min="1" max="99999" value= <?= $item['price'] ?>>
+			<br /> Quantity in stock:
+			<input type="number" name="quantity" min="0" max="99999" value= <?= $item['quantity'] ?>>
+			<br /> Category:
+			<input type="text" name="category" value= <?= $item['category'] ?>>
+			<button type="submit" name="submit" value=<?= $item['name'] ?>>Update product</button>
+			</form>
+			<br />
+		<?php
+		}
+	}
+?>
