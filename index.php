2a3f342e95ab7ac7959566b4bba94f69618eba2d
sadawi <hahli144@hotmail.com>
2020-06-28 18:56:07 +0300

    session basket now carries over when logging in

--- index.php
+++ index.php
@@ -101,10 +101,119 @@ function	get_item_max_quantity2($products, $cart_item)
 	return (0);
 }
 
-		$products = csv_to_array2("products.csv");
-		echo "<h2>Shopping Cart: </h2><br />";
+function csv_to_array_with_format2($filename)
+{
+	$arr = array_map("str_getcsv", file($filename));
+	array_walk($arr, function(&$item) use ($arr)
+	{
+		$item = array_combine($arr[0], $item);
+	});
+	return ($arr);
+}
+
+function save_item_to_cart2($item, $products, $amount)
+{
 	if ($_SESSION['logged_on_user'])
 	{
+		if (file_exists($cart_directory) === FALSE)
+			mkdir("shopping_carts");
+		if (!file_exists("shopping_carts/".$_SESSION['logged_on_user']."_cart.csv"))
+		{
+			$handle = fopen("products.csv", "r");
+			file_put_contents("shopping_carts/".$_SESSION['logged_on_user']."_cart.csv", fgets($handle));
+			fclose($handle);
+		}
+		$cart = csv_to_array_with_format2("shopping_carts/".$_SESSION['logged_on_user']."_cart.csv");
+		$item_found = 0;
+		foreach ($cart as &$cart_item)
+		{
+			if ($cart_item['name'] == $item['name'])
+			{
+				if (isset($_GET['new_quantity']))
+				{
+					if ($_GET['new_quantity'] == 0)
+						$cart_item = NULL;
+					else
+						$cart_item['quantity'] = $_GET['new_quantity'];
+				}
+				else
+				{
+					if ($cart_item['quantity'] + $amount <= $item['quantity'])
+					{
+						$cart_item['quantity'] += $amount;
+						$item_found = 1;
+					}
+					else
+					{
+						echo "<script>alert('Sorry, we are out of stock!');</script>";
+						$item_found = 1;
+					}
+				}
+				$item_found = 1;
+			}
+		}
+		if (!$item_found)
+		{
+			if ($item['quantity'] >= $amount)
+			{
+				$item['quantity'] = $amount;
+				$cart[] = $item;
+			}
+			else
+				echo "<script>alert('Sorry, we are out of stock!');</script>";
+		}
+		unset($cart_item);
+		$handle = fopen("shopping_carts/".$_SESSION['logged_on_user']."_cart.csv", "w");
+		foreach ($cart as $cart_item)
+		{
+			fputcsv($handle, $cart_item);
+		}
+		fclose($handle);
+	}
+	else
+	{
+		if (isset($_GET['new_quantity']))
+		{
+			if ($_GET['new_quantity'] == 0)
+				unset($_SESSION['basket'][$item['name']]);
+			else
+				$_SESSION['basket'][$item['name']] = $_GET['new_quantity'];
+		}
+		else
+		{
+			if ($_SESSION['basket'][$item['name']] < $item['quantity'])
+				$_SESSION['basket'][$item['name']]++;
+			else
+				echo "<script>alert('Sorry, we are out of stock!');</script>";
+		}
+	}
+}
+
+function replace_basket($products)
+{
+	$found = 0;
+	foreach ($products as $item)
+	{
+		if ($_SESSION['basket'][$item['name']])
+			$found = 1;
+	}
+	if ($found)
+	{
+		unlink("shopping_carts/".$_SESSION['logged_on_user']."_cart.csv");
+		foreach ($products as $item)
+		{
+			if ($_SESSION['basket'][$item['name']])
+				save_item_to_cart2($item, $products, $_SESSION['basket'][$item['name']]);
+		}
+	}
+	unset($_SESSION['basket']);
+}
+
+	$products = csv_to_array2("products.csv");
+	echo "<h2>Shopping Cart: </h2><br />";
+	if ($_SESSION['logged_on_user'])
+	{	
+		replace_basket($products);
 		$cart = csv_to_array2("shopping_carts/".$_SESSION['logged_on_user']."_cart.csv");
 		$total_price = 0;
 		foreach ($cart as $item)
