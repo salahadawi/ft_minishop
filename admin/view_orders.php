5c9bf9150ceb49897ce0aeeed6f52fcc80e27905
sadawi <hahli144@hotmail.com>
2020-06-28 17:48:21 +0300

    add ability to delete orders

--- admin/view_orders.php
+++ admin/view_orders.php
@@ -6,8 +6,15 @@
 
 include("functions.php");
 session_start();
+
+if ($_GET['delete'])
+{
+	unlink("orders/".$_GET['delete']);
+}
+
 $directory = "orders";
 $dir = array_diff(scandir("orders"), array('..', '.'));
+
 foreach ($dir as $file)
 {
 	$name = substr($file, 0, strpos($file, "_order"));
@@ -15,11 +22,18 @@ foreach ($dir as $file)
 	$cart = csv_to_array($directory."/".$file);
 	$total_price = 0;
 	foreach ($cart as $item)
-		{
-			echo $item['name'].", quantity: ".$item['quantity']."<br />";
-			$total_price += $item['price'] * $item['quantity'];
-		}
-		echo "<br /> Total price: ".$total_price."$ <br /><br /><br />";
+	{
+		echo $item['name'].", quantity: ".$item['quantity']."<br />";
+		$total_price += $item['price'] * $item['quantity'];
+	}
+	echo "<br /> Total price: ".$total_price."$ <br />";
+	?>
+	<form action="index.php" method="GET">
+	<button type="submit" name="delete" value=<?= $file ?>>Delete order</button>
+	<input type="hidden" name="page" value="admin/view_orders">
+	</form>
+	<br /><br />
+	<?php
 }
 if (empty($dir))
 	print("No orders have been placed yet!")
